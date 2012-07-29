3f02884fa6477ebe0cc3eca81d0585e8261f82d1
Maxwell Swadling <maxwellswadling@gmail.com>
2012-07-29 20:51:24 +1000

    Initial rails template

---
+++ README.md
@@ -0,0 +1,3 @@
+# Event Manager
+
+Manage events.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EventManager::Application.config.secret_token = 'c904aef6e4288acb630ab06ecc988de9f6bc92a5eccbc416d2d9f80db9ab73208212ec68c58086251b7ace53dff25a4165742360f1855710c766d19275134d13'
