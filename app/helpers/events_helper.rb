553826407968d3b49e27e25d2596742845bd3181
Maxwell Swadling <maxwellswadling@gmail.com>
2012-07-30 20:40:02 +1000

    Added errors

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -2,4 +2,8 @@ module EventsHelper
   def price_name(x)
     "#{x.name} (#{number_to_currency(x.price / 100)})"
   end
+  
+  def filter_errors(x)
+    x.full_messages.uniq.reject{|x| x == 'Tickets is invalid'}
+  end
 end
