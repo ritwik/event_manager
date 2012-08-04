4fccd819d0f91c72c158358aa2fc53c6060ad330
Maxwell Swadling <maxwellswadling@gmail.com>
2012-08-04 21:59:06 +1000

    Added mail to file for development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,6 +15,9 @@ EventManager::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
+  
+  # Put all mail into a file
+  config.action_mailer.delivery_method = :file
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
